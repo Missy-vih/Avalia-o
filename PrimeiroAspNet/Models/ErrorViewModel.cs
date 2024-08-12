06542b74c6386a133e984897e90023019f22056d
Missy-vih <vgrs530@gamil.com>
2024-08-12 15:26:44 -0300

    terceiro commit

--- PrimeiroAspNet/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PrimeiroAspNet.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
